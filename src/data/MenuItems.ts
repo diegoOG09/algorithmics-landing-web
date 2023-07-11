7904f132891e016afc116ef760ba6054ea94c510
diegoOG09 <xdiegoemmax@outlook.com>
2023-07-10 23:08:52 -0600

    video principal colocado

---
+++ src/data/MenuItems.ts
@@ -0,0 +1,10 @@
+import type { MenuItem } from "../types/types";
+
+const menuItems : MenuItem[] = [
+  {
+    title: 'Agendar clase muestra',
+    url: 'https://deluxe-parfait-f5ff57.netlify.app/'
+  }
+]
+
+export default menuItems;
