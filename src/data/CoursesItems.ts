4cd8c5ea95c9c6c7feda4c9eca48a391a85f14b7
diegoOG09 <xdiegoemmax@outlook.com>
2023-07-12 01:22:02 -0600

    Add courses to main section

---
+++ src/data/CoursesItems.ts
@@ -0,0 +1,26 @@
+import type { CoursesItem } from "../types/types";
+
+const coursesItems : CoursesItem[] = [
+  {
+    age: 'Para 6-7 años',
+    course: 'Fundamentos de lógica y programación'
+  },
+  {
+    age: 'Para 9-10 años',
+    course: 'Programación visual'
+  },
+  {
+    age: 'Para 10-11 años',
+    course: 'Diseño de videojuegos'
+  },
+  {
+    age: 'Para 12-13 años',
+    course: 'Python Start'
+  },
+  {
+    age: 'Para 14-17 años',
+    course: 'Python Pro'
+  }
+]
+
+export default coursesItems;
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,8 @@ export interface MenuItem {
   title: string;
   url: string;
 }
+
+export interface CoursesItem {
+  age: string;
+  course: string;
+}
