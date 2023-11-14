68b125347687cec77fddfaa1b55fb80509403f5c
diegoOG09 <xdiegoemmax@outlook.com>
2023-11-13 21:35:44 -0600

    Add a new page for each course

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,6 +7,12 @@ export interface CoursesItem {
   age: string;
   course: string;
   image: string;
+  link: string;
+  color: string;
+  border_color: string;
+  title_color: string;
+  p_color: string;
+  button_color: string;
 }
 
 export interface CoursesInfoItem {
