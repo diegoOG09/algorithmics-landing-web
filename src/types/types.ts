c5c4f62215961e099060dae4ead97bc7893ae837
diegoOG09 <xdiegoemmax@outlook.com>
2023-11-15 22:25:53 -0600

    Latest update

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,8 +14,19 @@ export interface CoursesItem {
   p_color: string;
   button_color: string;
   text_button_color: string;
-  // content: string;
-  // metodology: string;
+  content_1: string;
+  content_2: string;
+  content_3: string;
+  content_4: string;
+  metodology_1: string;
+  metodology_2: string;
+  metodology_3: string;
+  online_1: string;
+  online_2: string;
+  online_3: string;
+  inperson_1: string;
+  inperson_2: string;
+  inperson_3: string;
 }
 
 export interface CoursesInfoItem {
